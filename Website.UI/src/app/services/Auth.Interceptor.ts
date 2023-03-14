4605b736acb70b28733a707e823aefd1055ec6ab
mahan <mahan.timalsena@danphecare.com>
2023-03-14 17:14:09 +0545

    Added feature of auto logout when the jwt token expires.

--- Website.UI/src/app/services/Auth.Interceptor.ts
+++ Website.UI/src/app/services/Auth.Interceptor.ts
@@ -1,26 +1,35 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import {  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 import { enviroment } from "src/enviroments/enviroment";
 import { AuthenticationService } from "./authentication.service";
+import jwt_decode from 'jwt-decode';
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
-    constructor(private _authenticationService: AuthenticationService){
-
-    }
+    constructor( private _authenticationService: AuthenticationService, private _router: Router ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const isLoggedIn = this._authenticationService.isLoggedIn();
         const isApiUrl = req.url.startsWith(enviroment.baseApiUrl);
         const token = sessionStorage.getItem('authToken');
-        if(isLoggedIn && isApiUrl){
-            req = req.clone({
-                setHeaders:{
-                    Authorization: `Bearer ${token}`
-                }
-            })
-        }
+        if(isLoggedIn && isApiUrl && token){
+            const decodedToken:any = jwt_decode(token);
+            if(decodedToken.exp < Date.now() / 1000 ){
+                sessionStorage.removeItem('authToken');
+                this._authenticationService.logOut();
+                this._router.navigate(['/login']);
+               
+             }else{
+                req = req.clone({
+                    setHeaders:{
+                        Authorization: `Bearer ${token}` 
+                    }})
+            }
+           
+        };
         return next.handle(req);
+        
     }
 }
